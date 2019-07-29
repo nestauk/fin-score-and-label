6fb29eb0846a0f8b5a1b9d68a521f6e02805e261
tghudson <giblets@Toms-MacBook-Pro-2.local>
2019-07-29 20:23:22 +0100

    tests for all the functions

--- tests/test_score_label.py
+++ tests/test_score_label.py
@@ -1,12 +1,12 @@
 import json
 import numpy as np
 import pytest
-from nesta_score_label.nesta_score_label import create_feature_matrix, calculate_scores, calculate_labels
+from sklearn.ensemble.forest import RandomForestClassifier
+from nesta_score_label.nesta_score_label import *
 
 @pytest.fixture
 def mocked_keywords():
     return ['one', 'two', 'three', 'four']
-
 
 @pytest.fixture
 def mocked_entry():
@@ -20,6 +20,22 @@ def real_keywords():
     with open('./nesta_score_label/models/saved_keywords.csv') as file:
         keywords = [w for w in file.read().split('\n')[1:] if len(w) > 0]
     return keywords
+
+@pytest.fixture
+def team_names():
+    return [
+        'Innovation Mapping',
+        'RAP Innovation',
+        'RAP International Innovation',
+        'RAP Explorations',
+        'Creative Economy (RAP and IP)',
+        'Challenge Prizes',
+        'Health Lab',
+        'Skills',
+        'RAP Explorations',
+        'Innovation Programmes - Education',
+        'Government Innovation (RAP and IP)'
+    ]
 
 def test_successful_feature_matrix_created(mocked_entry, mocked_keywords):
     mocked_entries = [mocked_entry]
@@ -59,3 +75,49 @@ def test_successful_calculate_labels_created(mocked_entry, real_keywords):
 
     # check the size of the output is correct
     assert labels.shape[0] == len(mocked_entries)
+
+def test_load_model(team_names):
+    nesta_model = load_model("nesta_model.pkl")
+    assert type(nesta_model) == RandomForestClassifier
+
+    for name in team_names:
+        model = load_model("team_model_" + name + ".pkl")
+        assert type(model) == RandomForestClassifier
+
+def test_load_keywords(real_keywords):
+    keywords = load_keywords()
+    assert len(keywords) == len(real_keywords)
+    assert set(keywords) == set(real_keywords)
+
+def test_len_tokenized_text():
+    l = len_tokenized_text("this is a great set of words")
+    assert l == 7
+
+def test_get_counting_cols():
+
+    df = pd.DataFrame({
+        'text': ['this is a list of keywords where keywords are good'],
+    })
+
+    keywords = ['keywords', 'list']
+
+    x = get_counting_cols(df, 'text', keywords)
+
+    assert set(x.columns.tolist()) == set(['text_count_' + s for s in keywords])
+    assert x.iloc[0].text_count_keywords == 2
+    assert x.iloc[0].text_count_list == 1
+
+def test_tidy_text():
+    text = "WHERE IS the 1 THING\n\r\n THAT I NEED!!!!!"
+    ttext = tidy_text(text)
+    assert ttext == "where is the 1 thing   that i need"
+
+def test_binary_cols_to_concat_text(team_names):
+
+    pred_teams = ['pred_' + s for s in team_names]
+
+    df = pd.DataFrame({team: [1] for team in pred_teams})
+
+    label = binary_cols_to_concat_text(df.iloc[0], pred_teams, team_names)
+
+    assert label == ', '.join(teams)
